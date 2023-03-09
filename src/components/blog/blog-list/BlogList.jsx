06da49b1d19ac764172cc47f7d8afa6c8933ad05
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-03-09 17:54:42 +0000

    front end connected to back end

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  
+  const getBlogPosts = async () => {
+    try {
+      let response = await fetch("http://localhost:3001/blogPosts")
+      if (response.ok) {
+        let data = await response.json()
+        console.log(data)
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {getBlogPosts()}, [])
+  
   return (
     <Row>
       {posts.map((post) => (
